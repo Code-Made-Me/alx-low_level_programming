1b40dafd5c6ff5ec539cd89e8a7b73c7202c3f2a
Code-Made-Me <nthabiseng.nnkuna@gmail.com>
2023-12-18 14:14:22 +0200

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ *  main - Entry point of the program
+ *
+ * description: print a quote
+ *
+ * Return: Always returns 1 to indicate an error
+*/
+int main(void)
+{
+char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+return (1);
+}
