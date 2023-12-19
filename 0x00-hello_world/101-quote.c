c44cab3f314ee4de756e4408c37c899465473647
Code-Made-Me <nthabiseng.nnkuna@gmail.com>
2023-12-19 06:22:47 +0200

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-
 /**
- *  main - Entry point of the program
+ * main - printing without printf and puts
  *
- * description: print a quote
+ * Return: 1 (Success)
  *
- * Return: Always returns 1 to indicate an error
-*/
+ */
 int main(void)
 {
-char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, strlen(message));
 return (1);
 }
